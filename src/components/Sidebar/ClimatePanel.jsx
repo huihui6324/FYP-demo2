2ec5ec4588446b38665580d1561a0cc3ee65d306
huihui6324 <1982806447@qq.com>
2026-04-17 18:31:06 +0800

    refactor

--- src/components/Sidebar/ClimatePanel.jsx
+++ src/components/Sidebar/ClimatePanel.jsx
@@ -1,6 +1,6 @@
-// src/components/Sidebar/ClimatePanel.jsx
-import React, { useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import * as Cesium from 'cesium'
+import './Sidebar.css'
 
 export default function ClimatePanel({ climate, setClimate, viewer, isExpanded, onToggle }) {
   const rainEffectRef = useRef(null)
